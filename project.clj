0cf5a4650e4390b4eea1031a49e163a188a90c6b
Dominik Bylica <byldominik+fenoloftaleina@gmail.com>
2017-04-19 23:02:28 +0200

    Take ownership because of the clojars deployment requirements.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject trystan/delaunay-triangulation "1.0.2"
+(defproject fenoloftaleina/delaunay-triangulation "1.0.2"
   :description "Create a delaunay triangulation from a collection of points."
-  :url "https://github.com/trystan/delaunay-triangulation"
+  :url "https://github.com/fenoloftaleina/delaunay-triangulation"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
